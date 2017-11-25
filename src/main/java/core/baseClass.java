29f95c0419a89b8c2a7852d77f0df36b19a74076
namburi <namburi.sampath@gmail.com>
2017-11-25 10:33:17 -0500

    Add files via upload

---
+++ src/main/java/core/baseClass.java
@@ -0,0 +1,10 @@
+package core;
+
+/*
+    This is a Base Class that hold global methods which can be used in the project by extending this class
+ */
+public class baseClass {
+    public void whoAmI(){
+        System.out.println("I am Base Class");
+    }
+}
