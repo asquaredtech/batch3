e5e50ca6d074b978df36986c73a9ecec76460dc6
namburi <namburi.sampath@gmail.com>
2017-11-26 18:55:22 -0500

    Add files via upload

--- src/main/java/core/baseClass.java
+++ src/main/java/core/baseClass.java
@@ -2,9 +2,14 @@ package core;
 
 /*
     This is a Base Class that hold global methods which can be used in the project by extending this class
+    Base class is also called super class
  */
 public class baseClass {
     public void whoAmI(){
-        System.out.println("I am Base Class");
+        System.out.println("I am in Super or Base Class");
+    }
+
+    public void iLearned(){
+        System.out.print("I learned ");
     }
 }
