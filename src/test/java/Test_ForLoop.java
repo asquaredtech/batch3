8a8e1a757227546e944156600b5df82f21eeee57
namburi <namburi.sampath@gmail.com>
2017-12-19 16:50:19 -0500

    Update Test_ForLoop.java

--- src/test/java/Test_ForLoop.java
+++ src/test/java/Test_ForLoop.java
@@ -1,13 +1,22 @@
+import core.TestLib;
 import core.baseClass;
 import org.junit.Test;
 
-public class Test_ForLoop extends baseClass {
+public class Test_ForLoop extends TestLib {
     @Test
     public void testFor() {
 
-        for(int i = 1; i < 10; i = i+1) {
-            iLearned();
-            System.out.println("Table : " + i );
-        }
+        for (int i = 1; i < 10; i = i + 1) {
+            if (i % 2 == 1) { // 'i%2' will give the reminder
+                // odd number
+                iLearned(i + ": Valley Ridge Academy");//String concatenation
+            } // end if
+            else {
+                //even number
+                System.out.print(i + ": ");
+                iLearned();
+            } // end else
+            System.out.println("Table : " + i);
+        } // end for
     }
 }
